ddc33cb3dac9da487a18fd8c8a868c7642e7732a
lw <me@lw.ag>
2018-08-31 14:51:55 +0200

    Remove obsolete file.

--- test.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-const { copyFolder, removeFolder } = require('./src/utils');
-
-// copyFolder(path.resolve('public'), path.resolve('public2'));
-
-removeFolder(path.resolve('public2'));
