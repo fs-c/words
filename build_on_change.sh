e815cc0689b94db8d113518d5c741706bec1bd80
lw <me@lw.ag>
2018-08-31 14:29:25 +0200

    Add automatic dir watching and building script for convenience

---
+++ build_on_change.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+DIRECTORY_TO_OBSERVE="./"
+
+function block_for_change {
+	inotifywait -r \
+		-e modify,move,create,delete \
+		$DIRECTORY_TO_OBSERVE 2> /dev/null
+}
+
+BUILD_SCRIPT="node words.js"
+
+function build {
+	eval $BUILD_SCRIPT
+}
+
+build
+while block_for_change; do
+	build
+done
